fdbee580e24f25b82fe7f07472477573215b6527
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-12-07 10:22:30 -0800

    Ceneter and Text widget
    Center and Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';//material package
 void main(){
   runApp(Myapp());
-  
 }
 class Myapp extends StatelessWidget {
   const Myapp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container();
