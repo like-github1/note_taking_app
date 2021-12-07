e33958b86224bbc8429d715a879c9ff5a1a6705e
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-12-07 09:55:04 -0800

    flutter_hello_world

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';//material package
+void main(){
+  runApp(Myapp());
+  
+}
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
